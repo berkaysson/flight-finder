a05249ca768b599b7abc2caf4a8e1a1a801751a1
berkaysson <berkaysonel85@gmail.com>
2024-08-17 21:27:44 +0300

    feature: arayüze uçuş yönü seçimi eklendi

--- frontend/src/components/FlightFilter.tsx
+++ frontend/src/components/FlightFilter.tsx
@@ -1,21 +1,50 @@
 import styled from "styled-components";
 import Card from "./ui/Card";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Plane, PlaneLanding, PlaneTakeoff, CalendarFold } from "lucide-react";
+import { FlightContext } from "../context/FlightsContext";
 
 const FlightFilter = () => {
+  const { changeFlightDirection } = useContext(FlightContext);
+
   const [tripType, setTripType] = useState("Round Trip");
+  const [from, setFrom] = useState<"A" | "D">("D");
+  const [to, setTo] = useState<"A" | "D">("A");
+
+  const handleFromChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value as "A" | "D";
+    setFrom(value);
+    if (value === "D") {
+      setTo("A");
+    } else {
+      setTo("D");
+    }
+  };
+
+  const handleToChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value as "A" | "D";
+    setTo(value);
+    if (value === "D") {
+      setFrom("A");
+    } else {
+      setFrom("D");
+    }
+  };
+
+  useEffect(() => {
+    changeFlightDirection(from);
+  }, [from]);
 
   return (
     <Card>
-      <div className="flex flex-col gap-4 justify-between mb-4 md:flex-row">
-        <h2 className="text-lg font-bold flex gap-2 items-center">
+      <div className="flex flex-col justify-between gap-4 mb-4 md:flex-row">
+        <h2 className="flex items-center gap-2 text-lg font-bold">
           <Plane size={20} />
           BOOK YOUR FLIGHT
         </h2>
         <div className="inline-flex font-bold">
           <TripTypeButton
-            className="rounded-l-full border-r-0"
+            className="border-r-0 rounded-l-full"
             onClick={() => setTripType("Round Trip")}
             active={tripType === "Round Trip"}
           >
@@ -24,42 +53,58 @@ const FlightFilter = () => {
           <TripTypeButton
             onClick={() => setTripType("One Way")}
             active={tripType === "One Way"}
-            className="rounded-r-full border-l-0"
+            className="border-l-0 rounded-r-full"
           >
             One Way
           </TripTypeButton>
         </div>
       </div>
 
-      <div className="flex flex-col gap-4 justify-between lg:flex-row items-center">
-        <div className="flex flex-col sm:flex-row gap-1 w-full">
+      <div className="flex flex-col items-center justify-between gap-4 lg:flex-row">
+        <div className="flex flex-col w-full gap-1 sm:flex-row">
           <InputWrapper>
             <PlaneTakeoff size={20} className="input-icon" />
-            <StyledSelect className="sm:rounded-l-full sm:border-r-0 rounded-t-xl">
-              <option>From</option>
+            <StyledSelect
+              value={from}
+              onChange={handleFromChange}
+              className="sm:rounded-l-full sm:border-r-0 rounded-t-xl"
+            >
+              <option value="D">Amsterdam</option>
+              <option value="A">Other</option>
             </StyledSelect>
           </InputWrapper>
           <InputWrapper>
             <PlaneLanding size={20} className="input-icon" />
-            <StyledSelect className="sm:rounded-r-full sm:border-l-0 rounded-b-xl">
-              <option>To</option>
+            <StyledSelect
+              value={to}
+              onChange={handleToChange}
+              className="sm:rounded-r-full sm:border-l-0 rounded-b-xl"
+            >
+              <option value="A">Other</option>
+              <option value="D">Amsterdam</option>
             </StyledSelect>
           </InputWrapper>
         </div>
-        <div className="flex flex-col sm:flex-row gap-1 w-full">
+        <div className="flex flex-col w-full gap-1 sm:flex-row">
           <InputWrapper>
             <CalendarFold size={20} className="input-icon" />
-            <StyledInput type="date" className="sm:rounded-l-full sm:border-r-0 rounded-t-xl" />
+            <StyledInput
+              type="date"
+              className="sm:rounded-l-full sm:border-r-0 rounded-t-xl"
+            />
           </InputWrapper>
           <InputWrapper>
             <CalendarFold size={20} className="input-icon" />
-            <StyledInput type="date" className="sm:rounded-r-full sm:border-l-0 rounded-b-xl" />
+            <StyledInput
+              type="date"
+              className="sm:rounded-r-full sm:border-l-0 rounded-b-xl"
+            />
           </InputWrapper>
         </div>
       </div>
 
       <div className="flex m-2 mt-4">
-        <button className="bg-theme text-white py-2 px-4 rounded-lg hover:opacity-80">
+        <button className="px-4 py-2 text-white rounded-lg bg-theme hover:opacity-80">
           Show Flights
         </button>
       </div>
