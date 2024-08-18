b93d7ef30045250c0ab4b951158796704c8b1a12
berkaysson <berkaysonel85@gmail.com>
2024-08-18 22:36:37 +0300

    release: v1

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,29 +1,8 @@
 import axios from "axios";
-import axiosRetry from "axios-retry";
-
-export const schipholAxiosInstance = axios.create({
-  baseURL: "https://api.schiphol.nl/public-flights",
-  headers: {
-    Accept: "application/json",
-    resourceversion: "v4",
-    app_id: import.meta.env.VITE_SCHIPHOL_APP_ID,
-    app_key: import.meta.env.VITE_SCHIPHOL_APP_KEY,
-  },
-});
 
 export const backendAxiosInstance = axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
     Accept: "application/json",
-  },
-});
-
-axiosRetry(schipholAxiosInstance, {
-  retries: 3,
-  retryDelay: (retryCount) => {
-    return axiosRetry.exponentialDelay(retryCount);
-  },
-  retryCondition: (error) => {
-    return error.response?.status === 429;
   },
 });
