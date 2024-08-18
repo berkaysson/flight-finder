108cf82f159e659e1b1dbab1e3240b6879fd85db
berkaysson <berkaysonel85@gmail.com>
2024-08-18 21:08:24 +0300

    refactor: loglar kaldırıldı

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -2,7 +2,6 @@ import { format, parse } from "date-fns";
 
 export const formatDateTime = (date: string) => {
   if (!date) return "";
-  console.log(date);
   const parsedDate = parse(date, "yyyy-MM-dd'T'HH:mm", new Date());
   return format(parsedDate, "yyyy-MM-dd'T'HH:mm:ss");
 };
