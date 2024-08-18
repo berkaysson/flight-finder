4cdb8256447bb705b3907437caebc67288cffd6f
berkaysson <berkaysonel85@gmail.com>
2024-08-18 19:18:37 +0300

    chore: uçuşlarıma yönlendirme eklendi

--- frontend/src/components/MyFlightsList.tsx
+++ frontend/src/components/MyFlightsList.tsx
@@ -1,12 +1,23 @@
 import { useContext } from "react";
 import { FlightContext } from "../context/FlightsContext";
 import MyFlightListItem from "./MyFlightsListItem";
+import { Link } from "react-router-dom";
 
 const MyFlightsList = () => {
   const { myFlights } = useContext(FlightContext);
 
   return (
     <ul className="flex flex-col gap-4">
+      {myFlights.length === 0 && (
+        <>
+          <li className="text-sm text-textAlt">You have no flights.</li>
+          <li>
+            <Link to="/" className="font-bold underline text-theme hover:cursor-pointer">
+              Book some flights
+            </Link>
+          </li>
+        </>
+      )}
       {myFlights.map((flight, index) => {
         return <MyFlightListItem key={flight.id + index} flight={flight} />;
       })}
