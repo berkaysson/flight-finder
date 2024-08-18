c3fbb8f79ad2ee4c6990ab8e045ffd1d4b07b86d
berkaysson <berkaysonel85@gmail.com>
2024-08-18 22:35:20 +0300

    release: v1

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const flightRoutes = require("./flightRoutes");
+const schipnolRoutes = require("./schipholRoutes");
 
 const router = express.Router();
 
 router.use("/flight", flightRoutes);
+router.use("/schiphol", schipnolRoutes);
 
 module.exports = router;
