1436a3db851883d09b0d3270f013c3894d6cb4c3
berkaysson <berkaysonel85@gmail.com>
2024-08-18 19:39:38 +0300

    feature: uçuşlarımda lokasyon bazlı filtreleme için arayüz eklendi

--- frontend/src/components/MyFlightsFilter.tsx
+++ frontend/src/components/MyFlightsFilter.tsx
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+import { FlightContext } from "../context/FlightsContext";
+
 const MyFlightsFilter = () => {
-  return <div>MyFlightsFilter</div>;
+  const { myFlights } = useContext(FlightContext);
+
+  const bookedDestinations = Array.from(
+    new Set(myFlights.flatMap((flight) => [flight.departure, flight.arrival]))
+  );
+
+  return (
+    <div className="flex flex-row flex-wrap gap-3 mt-6">
+      {bookedDestinations.map((destination) => (
+        <button
+          className="px-4 py-2 text-sm border border-gray-200 border-solid rounded-lg shadow-sm hover:text-textAlt bg-text"
+          key={destination}
+        >
+          {destination}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default MyFlightsFilter;
