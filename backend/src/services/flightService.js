db6571a2c4bec060ea3f6fc6580a6e5930d012c4
berkaysson <berkaysonel85@gmail.com>
2024-08-18 19:19:18 +0300

    fix: aynı uçuşların veritabanına eklenmesi engellendi

--- backend/src/services/flightService.js
+++ backend/src/services/flightService.js
@@ -1,6 +1,12 @@
 const Flight = require("../models/Flight");
 
 exports.createFlight = async (flightData) => {
+  const existingFlight = await Flight.findOne({ id: flightData.id });
+
+  if (existingFlight) {
+    return existingFlight;
+  }
+
   return await Flight.create(flightData);
 };
 
